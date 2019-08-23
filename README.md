0190cacb48bf11654e7cf71388c50a336f6e75c5
Vemund Santi <vemund@santi.no>
2019-08-23 11:38:31 +0200

    feat: implement plugin that parses Spotify URIs and URLs into iframes
    Widget for CKEditor 4.3+ which provides a convenient dialog window for
    inserting an iframe with a Spotify Play Button (https://developer.spotify.com/documentation/widgets/generate/play-button/).
    
    Supports album, artist, playlist and track Play Buttons.
    Uses regex matchings to parse URIs and URLs.
    Supports none, left, right and centering alignment.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ckeditor-plugin-spotify
-Spotify plugin for CKEditor 4.x
+Spotify plugin for `CKEditor 4.3+`
+
+Requires plugin: `widget`
+
+CKEditor must allow: `div`, `iframe`
---
+++ spotify/dialogs/spotify.js
@@ -0,0 +1,91 @@
+CKEDITOR.dialog.add( 'spotify', function( editor ) {
+    return {
+        title: 'Edit Spotify Playlist',
+        minWidth: 200,
+        minHeight: 100,
+        contents: [
+            {
+                id: 'spotify',
+                elements: [
+                    {
+                        id: 'embedCode',
+                        type: 'text',
+                        label: 'Embed code', //TODO: i11n
+                        width: '200px',
+                        setup: function(widget) {
+                            this.setValue(widget.data.embedCode);
+                        },
+                        commit: function(widget) {
+                            widget.setData('embedCode', this.getValue())
+                        },
+                        validate: validateEmbedCode('Invalid Spotify embed code.'), // TODO: i11n
+                    },
+                    {
+                        id: 'align',
+                        type: 'select',
+                        label: 'Align', //TODO: i11n
+                        items: [
+                            [ editor.lang.common.notSet, '' ],
+                            [ editor.lang.common.alignLeft, 'left' ],
+                            [ editor.lang.common.alignRight, 'right' ],
+                            [ editor.lang.common.alignCenter, 'center' ]
+                        ],
+                        setup: function(widget) {
+                            this.setValue(widget.data.align);
+                        },
+                        commit: function(widget) {
+                            widget.setData('align', this.getValue())
+                        }
+                    },
+                    {
+                        type: 'hbox',
+                        widths: ['50%', '50%'],
+                        children: [
+                            {
+                                id: 'width',
+                                type: 'text',
+                                label: 'Width', //TODO: i11n
+                                width: '100px',
+                                setup: function(widget) {
+                                    this.setValue(widget.data.width);
+                                },
+                                commit: function(widget) {
+                                    widget.setData('width', this.getValue())
+                                }
+                            },
+                            {
+                                id: 'height',
+                                type: 'text',
+                                label: 'Height', //TODO: i11n
+                                width: '100px',
+                                setup: function(widget) {
+                                    this.setValue(widget.data.height);
+                                },
+                                commit: function(widget) {
+                                    widget.setData('height', this.getValue())
+                                }
+                            },
+                        ]
+                    },
+                    
+                ]
+            }
+        ]
+    };
+});
+
+
+function validateEmbedCode(errorMsg) {
+
+    // spotify:track:6rqhFgbbKwnb9MLmUQDhG6
+    var spotifyURIRegex = /^spotify:(album|playlist|track|artist):([a-zA-Z0-9]{22})$/
+    // https://open.spotify.com/album/4RuzGKLG99XctuBMBkFFOC
+    var spotifyURLRegex = /^https?:\/\/open\.spotify\.com\/(album|playlist|track|artist)\/([a-zA-Z0-9]{22})$/
+    
+    return function () {
+        var embedCode = this.getValue()
+
+        return spotifyURIRegex.test(embedCode) || spotifyURLRegex.test(embedCode) || errorMsg;
+    }
+
+}
